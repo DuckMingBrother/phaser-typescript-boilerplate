a95625dd61adeee05d4ce4b9b161626f26fccddc
duckmingbrother <soar75514@hotmail.com>
2017-08-26 23:44:26 +0800

    update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,32 @@
 import 'p2';
 import 'pixi';
+// tslint:disable-next-line:ordered-imports
 import 'phaser';
+import { CANVAS, Game, State } from 'phaser-ce';
 
-import { Game, AUTO } from 'phaser-ce';
+const assetKeys = {};
+Object.keys(assetKeys).forEach(k => import(`../assets/${k}.png`).then(img => assetKeys[k] = img));
 
 window.onload = () => {
-    const game = new Game(800, 600, AUTO, null, {
-        preload() {
+    const game = new Game(640, 480, CANVAS, 'game');
 
-        },
+    class PhaserGame extends State {
+        init() {
+
+        }
+
+        preload() {
+            Object.keys(assetKeys).forEach(k => this.load.image(k, assetKeys[k]));
+        }
+
         create() {
 
-        },
+        }
+
         update() {
 
         }
-    });
+    }
+
+    game.state.add('Game', PhaserGame, true);
 };
